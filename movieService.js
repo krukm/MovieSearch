5550b14a0cd42f91fbe630aab49ed3125876b57f
Adrean1983 <adrean_@hotmail.com>
2018-08-21 16:10:25 -0400

    some API Gathering done

--- movieService.js
+++ movieService.js
@@ -1,8 +1,17 @@
 "use strict";
-function movieService() {
+function MovieService($http) {
+    const vm = this;
+    vm.results = null;
+    vm.searchMovies = function (searchInput) {
+        return $http({
+            url: `https://api.themoviedb.org/3/movie/76341?api_key={aef5248d110c426d0fbb272768a4ff38}`,
+            method: "GET"
+        });
+        
+    }
 
 }
 
 angular
     .module("app")
-    .service("movieService", movieService )
+    .service("MovieService", MovieService);
