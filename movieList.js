fd0bf09c655671dc5a86cc579514ae072c08245b
Adrean1983 <adrean_@hotmail.com>
2018-08-21 20:45:55 -0400

    added API to show popular films on front page. Title/Poster/Link

--- movieList.js
+++ movieList.js
@@ -1,16 +1,35 @@
 "use strict"
 const movieList = {
-    template:`<p>Adrean</p>`,
-
+    template:`
+    <section ng-repeat="movie in $ctrl.popMovies | limitTo:10">
+    <p>{{ movie.title }}<p>
+    <img src="https://image.tmdb.org/t/p/w500/{{movie.poster_path}}"></img>
+    <a href="https://www.themoviedb.org/movie/{{movie.id}}">Link</a>
+    </section>
+    <section ng-repeat="movie in $ctrl.searchMovies | limitTo:10">
+    <p>{{ movie.title }}<p>
+    <img src="https://image.tmdb.org/t/p/w500/{{movie.poster_path}}"></img>
+    <a href="https://www.themoviedb.org/movie/{{movie.id}}">Link</a>
+    </section>
+    `,
+    
     controller: ["MovieService", function(MovieService) {
-      const vm = this;
+        const vm = this;
 
-    // put this inside a function
-    MovieService.searchMovies("matrix").then((response) => {
-        vm.searchMovies = response;
-        console.log(vm.searchMovies.overview);  
-    })
-
+        vm.displayOnPageOpen = () => {
+            vm.displayMovies("matrix");
+            
+        }
+        // put this inside a function
+        vm.displayMovies = (search) => {
+            MovieService.popularMovies().then((response) => {
+            vm.popMovies = response.results;
+            });
+            MovieService.searchMovies(search).then((response) => {
+            vm.searchMovies = response.results;
+            });
+        };
+        vm.displayOnPageOpen();
     }]
 }
 
