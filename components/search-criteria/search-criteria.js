16a84233f90b4384c2d51626b89b6c701e235bd3
Adrean1983 <adrean_@hotmail.com>
2018-08-23 12:06:02 -0400

    added 8 search filters

--- components/search-criteria/search-criteria.js
+++ components/search-criteria/search-criteria.js
@@ -17,6 +17,55 @@ const searchCriteria = {
                
             }  
         }
+        // Search by Specfic filters
+        //2018 Top Films
+        vm.getTopYearList = () => {
+            MovieService.topYearList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //Big Budget Films
+        vm.getBigBudgetList = () => {
+            MovieService.bigBudgetList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //Popular Kids films
+        vm.getKidFilmList = () => {
+            MovieService.kidFilmList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //top rated 90's films
+        vm.getNinetiesList = () => {
+            MovieService.ninetiesList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //top rated 80s films
+        vm.getEightiesList = () => {
+            MovieService.eightiesList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //french
+        vm.getFrenchList = () => {
+            MovieService.frenchList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //Japanese
+        vm.getJapaneseList = () => {
+            MovieService.japaneseList().then((response) => {
+            vm.result = response;
+            });
+        };
+        //romantic comedy
+        vm.getRomanticComedyList = () => {
+            MovieService.romanticComedyList().then((response) => {
+            vm.result = response;
+            });
+        };
     }]
 } 
 
