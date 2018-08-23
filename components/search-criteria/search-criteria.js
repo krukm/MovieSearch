c399744aee14908525f63b192f951a0347a14a35
Adrean1983 <adrean_@hotmail.com>
2018-08-22 22:32:23 -0400

    added genresearch with button

--- components/search-criteria/search-criteria.js
+++ components/search-criteria/search-criteria.js
@@ -5,18 +5,37 @@ const searchCriteria = {
     controller: ['SearchService', 'MovieService', 'WatchListService', function(SearchService, MovieService, WatchListService) {
         const vm = this;
         vm.result = null;
+        vm.genreList = null;
+        vm.selectedGenre = null;
     
         vm.getSearchResults = (searchTerm) => {
             MovieService.searchMovies(searchTerm).then((response) => {
                 vm.result = response;
                 SearchService.setSearchTerm(searchTerm);
-                console.log(vm.result);
             });
             vm.addToWatchlist = (movie) => {
                 WatchListService.addToWatchlist(movie);
                
             }  
         }
+
+        vm.getGenreList = () => {
+            MovieService.genreList().then((response) => {
+                vm.genreList = response.genres;
+            });
+        };
+        vm.getGenreResults = (genreInput) => {
+            MovieService.moviesByGenre(genreInput).then((response) => {
+                console.log(genreInput);
+                vm.result = response;
+                console.log(response)
+                console.log(vm.result)
+                SearchService.setSearchTerm(genreInput);
+            });
+        }
+        
+
+        vm.getGenreList();
     }]
 } 
 
--- services/movieService.js
+++ services/movieService.js
@@ -4,7 +4,7 @@ function MovieService($http) {
     vm.results = null;
     vm.searchMovies = function (searchInput) {
         return $http({
-            url: `https://api.themoviedb.org/3/search/movie?api_key=aef5248d110c426d0fbb272768a4ff38&query=${searchInput}`,
+            url: `https://api.themoviedb.org/3/search/movie?api_key=aef5248d110c426d0fbb272768a4ff38&query=${searchInput}&genre=family`,
             method: "GET"
         }).then((response) => {
             return response.data;
@@ -13,6 +13,22 @@ function MovieService($http) {
     vm.popularMovies = function () {
         return $http({
             url: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aef5248d110c426d0fbb272768a4ff38`,
+            method: "GET"
+        }).then((response) => {
+            return response.data;
+        });
+    }
+    vm.genreList = function () {
+        return $http({
+            url: `https://api.themoviedb.org/3/genre/movie/list?api_key=aef5248d110c426d0fbb272768a4ff38&language=en-US`,
+            method: "GET"
+        }).then((response) => {
+            return response.data;
+        });
+    }
+    vm.moviesByGenre = function (genreInput) {
+        return $http({
+            url: `https://api.themoviedb.org/3/discover/movie?api_key=aef5248d110c426d0fbb272768a4ff38&language=en-US&with_genres=${genreInput.id}`,
             method: "GET"
         }).then((response) => {
             return response.data;
